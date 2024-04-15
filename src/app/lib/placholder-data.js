dbdb80e8bc80e2b5de8a73ca87e789257f1d14bc
Brittany <brittsdevlog@gmail.com>
2024-04-15 09:36:56 -0500

    Adding inital

---
+++ src/app/lib/placholder-data.js
@@ -0,0 +1,25 @@
+
+
+const projects = [
+    {
+        project_title: "My cool react project",
+        location: "https://github.com/thebittles",
+        description: "My really cool project was built with xyz"
+    }
+  ];
+
+
+  const socialLinks = [
+    { 
+        social: "Github",
+        link: "https://github.com/thebittles",
+    },
+    { 
+        social: "LinkedIn",
+        link: "https://www.linkedin.com/in/brittany-sifford-3637041b6/",
+    },
+    { 
+        social: "Buy Me Coffee",
+        link: "https://www.buymeacoffee.com/brittsdevlk",
+    },
+  ]
---
+++ src/app/ui/fonts.js
@@ -0,0 +1,4 @@
+import { Micro_5 } from "next/font/google";
+
+
+export const Micro_5 = Micro_5({ subsets: ['latin']});
