44bcf20494b6454bb4274db9923e30c11416ab5b
Brittany <brittsdevlog@gmail.com>
2024-04-15 09:43:01 -0500

    Removing code to test deploy

--- src/app/ui/fonts.js
+++ src/app/ui/fonts.js
@@ -1,4 +1,4 @@
-import { Micro_5 } from "next/font/google";
+// import { Micro_5 } from "next/font/google";
 
 
-export const Micro_5 = Micro_5({ subsets: ['latin']});
+// export const Micro_5 = Micro_5({ subsets: ['latin']});
