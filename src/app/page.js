9d08a1773604fcc69412b0e33e43464a8883334d
Brittany <brittsdevlog@gmail.com>
2024-05-19 21:48:01 -0500

    Fixed file spelling

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import "./ui/bento.css";
-import BentoBox from "./components/bentoBox.js";
+import BentoBox from "./components/BentoBox.js";
 
 
 export default function Home() {
